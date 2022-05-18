81f73ca5075480641235dc3970628b5769304dd6
dev-fominov <dev-fominov@yandex.by>
2022-05-18 06:35:38 +0300

    Updates

---
+++ precache-manifest.d5ed500023ded5fed83c817872cd8cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e07b4cddb498916c8fa18d59a5f3ff",
+    "url": "/dz-ignat/index.html"
+  },
+  {
+    "revision": "d0b1712c1d8af02cdc53",
+    "url": "/dz-ignat/static/css/main.bd46bebf.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/dz-ignat/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dz-ignat/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b1712c1d8af02cdc53",
+    "url": "/dz-ignat/static/js/main.e7a3ab9d.chunk.js"
+  },
+  {
+    "revision": "b4703ce64f30f31053d8",
+    "url": "/dz-ignat/static/js/runtime-main.e8132bc0.js"
+  }
+]);
